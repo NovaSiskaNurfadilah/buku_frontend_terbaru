c0b0fbf232ecb28ec4c63bd0d425eeb1ee89dd1b
Nova Siska Nurfadilah <12150124481@students.uin-suska.ac.id>
2023-06-26 20:31:26 +0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+      <h1 class="text-white">BOOKSTORE NOPAII </h1>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.banner-image{
+  background: #0f58a1;
+  font-family: "Calibri Light" ;
+  font-style: italic;
+  font-size: 30px;
+
+}
+
+</style>
